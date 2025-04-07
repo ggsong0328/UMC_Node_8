5834d1a361ff2adcb056c9c00092ad6c97f9f2cd
ggsong0328 <ggsong0328@gmail.com>
2025-04-07 14:13:43 +0900

    프로젝트 세팅

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
+import cors from "cors";
 import express from "express"; // -> ES Module
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
